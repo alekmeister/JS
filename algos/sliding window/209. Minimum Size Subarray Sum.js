8e38dfc79715079f269ea82a434beedd5ef4a865
Alekmeister <olegpro6@gmail.com>
2026-05-07 12:45:25 +0300

    sliding window

--- algos/sliding window/209. Minimum Size Subarray Sum.js
+++ algos/sliding window/209. Minimum Size Subarray Sum.js
@@ -1,20 +1,4 @@
-// Given an array of positive integers nums and a positive integer target, return the minimal length of a subarray whose sum is greater than or equal to target.
-// If there is no such subarray, return 0 instead.
-// Example 1:
-//
-// Input: target = 7, nums = [2,3,1,2,4,3]
-// Output: 2
-// Explanation: The subarray [4,3] has the minimal length under the problem constraint.
-//     Example 2:
-//
-// Input: target = 4, nums = [1,4,4]
-// Output: 1
-// Example 3:
-//
-// Input: target = 11, nums = [1,1,1,1,1,1,1,1]
-// Output: 0
-
-
+//https://leetcode.com/problems/minimum-size-subarray-sum/description/
 
 /**
  * @param {number} target
@@ -24,7 +8,7 @@
 
 var minSubArrayLen = function(target, nums) {
 
-    let windowState = 0 // 15 14 12
+    let windowState = 0
     let begin = 0
     let result = Infinity
 
@@ -39,3 +23,8 @@ var minSubArrayLen = function(target, nums) {
     return result === Infinity ? 0 : result
 
 };
+minSubArrayLen(11, [1,2,3,4,5])
+
+//    s
+// [2,3,1,2,4,3]
+//        e
