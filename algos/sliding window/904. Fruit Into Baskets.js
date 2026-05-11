b8437e6260fddeaf8d8572da156d714cb944f4b5
Alekmeister <olegpro6@gmail.com>
2026-05-11 20:38:48 +0300

    sliding window

--- algos/sliding window/904. Fruit Into Baskets.js
+++ algos/sliding window/904. Fruit Into Baskets.js
@@ -1,54 +1,38 @@
-// You are visiting a farm that has a single row of fruit trees arranged from left to right. The trees are represented by an integer array fruits where fruits[i] is the type of fruit the ith tree produces.
-//
-//     You want to collect as much fruit as possible. However, the owner has some strict rules that you must follow:
-//
-//     You only have two baskets, and each basket can only hold a single type of fruit. There is no limit on the amount of fruit each basket can hold.
-//     Starting from any tree of your choice, you must pick exactly one fruit from every tree (including the start tree) while moving to the right. The picked fruits must fit in one of your baskets.
-//     Once you reach a tree with fruit that cannot fit in your baskets, you must stop.
-//     Given the integer array fruits, return the maximum number of fruits you can pick.
-//
-//
-//
-//     Example 1:
-//
-// Input: fruits = [1,2,1]
-// Output: 3
-// Explanation: We can pick from all 3 trees.
-//     Example 2:
-//
-// Input: fruits = [0,1,2,2]
-// Output: 3
-// Explanation: We can pick from trees [1,2,2].
-//     If we had started at the first tree, we would only pick from trees [0,1].
-//     Example 3:
-//
-// Input: fruits = [1,2,3,2,2]
-// Output: 4
-// Explanation: We can pick from trees [2,3,2,2].
-//     If we had started at the first tree, we would only pick from trees [1,2].
+//https://leetcode.com/problems/fruit-into-baskets
 
 
 
+/**
+ * @param {number[]} fruits
+ * @return {number}
+ */
 var totalFruit = function(fruits) {
 
-    //                *
+    // *
     // [3,3,3,1,2,1,1,2,3,3,4] //5
-    //        *
+    //  *
 
-    let start = 0,
-        windowState = new Map(),
+
+    let begin = 0,
+        windowSize = new Map(),
         result = 0
 
-    for (let end = 0; end < fruits.length; end++) {
-        windowState.set(fruits[end], (windowState.get(fruits[end]) || 0) + 1)
-        while(windowState.size > 2) {
-            const startFruit = fruits[start]
-            windowState.set(startFruit, windowState.get(startFruit) - 1)
-            if(windowState.get(startFruit) === 0) windowState.delete(startFruit)
-            start++
-        }
 
-        result = Math.max(result, end - start + 1)
+    for (let end = 0; end < fruits.length; end++ ) {
+        const currentVal = windowSize.has(fruits[end]) ? windowSize.get(fruits[end]) : 0
+
+        windowSize.set(fruits[end], currentVal + 1)
+
+        while(windowSize.size > 2) {
+            const winVal = windowSize.get(fruits[begin])
+            windowSize.set(fruits[begin], winVal - 1)
+            if(windowSize.get(fruits[begin]) === 0) {
+                windowSize.delete(fruits[begin])
+            }
+            begin++
+        }
+        result = Math.max(result, end - begin + 1)
+
     }
     return result
 };
