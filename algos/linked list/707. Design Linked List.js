d922c39866c9eff0849f721df1dafc187c471fe9
Alekmeister <olegpro6@gmail.com>
2026-05-20 17:34:16 +0300

    linked list

--- algos/linked list/707. Design Linked List.js
+++ algos/linked list/707. Design Linked List.js
@@ -1,8 +1,85 @@
-function Node(value, next) {
-    this.value = value;
-    this.next = next;
+class Node {
+    constructor(value, next) {
+        this.value = value
+        this.next = next
+    }
 }
 
+var MyLinkedList = function() {
+    this.head = new Node('dummy', null)
+};
+
+MyLinkedList.prototype.get = function(index) {
+    if (index < 0 || index >= this.getLength()) {
+        return -1
+    }
+
+    let current = this.head.next
+    for(let i = 0; i < index; i++) {
+        current = current.next
+    }
+    return current.value
+
+};
+
+
+MyLinkedList.prototype.addAtHead = function(val) {
+    this.head.next = new Node(val, this.head.next)
+};
+
+
+MyLinkedList.prototype.addAtTail = function(val) {
+    let current = this.head
+    while(current.next !== null) {
+        current = current.next
+    }
+    current.next = new Node(val, null)
+};
+
+
+MyLinkedList.prototype.addAtIndex = function(index, val) {
+    if (index < 0 || index > this.getLength()) {
+        return
+    }
+
+    let current = this.head
+
+    for (let i = 0; i < index; i++) {
+        current = current.next
+    }
+
+    const oldNext = current.next
+    current.next = new Node(val, oldNext)
+};
+
+
+MyLinkedList.prototype.deleteAtIndex = function(index) {
+    if (index < 0 || index >= this.getLength()) {
+        return
+    }
+
+    let current = this.head
+
+    for(let i = 0; i < index; i++) {
+        current = current.next
+    }
+    current.next = current.next.next
+};
+
+MyLinkedList.prototype.getLength = function() {
+    let length = 0
+    let current = this.head.next
+
+    while(current !== null) {
+        current = current.next
+        length++
+    }
+
+    return length
+};
+
+
+// без Dummy
 var MyLinkedList = function() {
     this.head = null
 };
@@ -92,3 +169,4 @@ list.addAtTail(4)
 list.addAtIndex(2, 2)
 list.deleteAtIndex(2)
 console.dir(list, { depth: null })
+
