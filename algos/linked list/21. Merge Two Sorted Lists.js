c727633b31bd6e39afd48e7394bd940c582d80a3
Alekmeister <olegpro6@gmail.com>
2026-06-03 14:04:33 +0300

    linked list

--- algos/linked list/21. Merge Two Sorted Lists.js
+++ algos/linked list/21. Merge Two Sorted Lists.js
@@ -1,3 +1,15 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
 var mergeTwoLists = function (list1, list2) {
     const dummy = new ListNode(-1)
 
@@ -5,6 +17,7 @@ var mergeTwoLists = function (list1, list2) {
 
     let p1 = list1,
         p2 = list2
+
 
     while (p1 && p2) {
         if (p1.val < p2.val) {
@@ -14,12 +27,16 @@ var mergeTwoLists = function (list1, list2) {
             current.next = p2
             p2 = p2.next
         }
-
         current = current.next
     }
 
-    if(p1) current.next = p1
-    if(p2) current.next = p2
+    if(p1) {
+        current.next = p1
+    }
+
+    if(p2) {
+        current.next = p2
+    }
 
     return dummy.next
 };
