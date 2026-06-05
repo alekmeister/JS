109f33ddd62f54973baa7a3174de6c959a0ce0a7
Alekmeister <olegpro6@gmail.com>
2026-06-05 12:31:21 +0300

    linked list

--- algos/linked list/141. Linked List Cycle.js
+++ algos/linked list/141. Linked List Cycle.js
@@ -11,6 +11,7 @@ var hasCycle = function(head) {
     while(fast && fast.next) {
         slow = slow.next
         fast = fast.next.next
+
         if(slow === fast) return true
     }
     return false
