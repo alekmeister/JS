f18cf4d5598cf205a4be3bca7bb4e984b88c2c52
Alekmeister <olegpro6@gmail.com>
2026-06-20 17:18:53 +0300

    stack

--- algos/tree/104. Maximum Depth of Binary Tree - dfs.js
+++ algos/tree/104. Maximum Depth of Binary Tree - dfs.js
@@ -1,34 +1,27 @@
 // https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
 
 var maxDepth = function(root) {
-    if(!root) {
-        return 0
-    }
+    if(!root) return 0;
 
     let left = maxDepth(root.left)
     let right = maxDepth(root.right)
 
     return Math.max(left, right) + 1
+}
 
-};
 
 //stack
 var maxDepth2 = function(root) {
-    if(!root) {
-        return 0
-    }
-
     const stack = [[root, 1]]
     let result = 0
 
-    while(stack.length > 0) {
+    while (stack.length) {
         const [node, depth] = stack.pop()
+        if(!node) continue;
+        result = Math.max(depth, result)
 
-        if(node) {
-            result = Math.max(result, depth)
-            stack.push([node.left, depth + 1])
-            stack.push([node.right, depth + 1])
-        }
+        stack.push([node.left, depth + 1])
+        stack.push([node.right, depth + 1])
     }
     return result
-};
+}
