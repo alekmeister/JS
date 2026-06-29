1a9bd11c21f5996e12652929ce1309f3d7cca72d
Alekmeister <olegpro6@gmail.com>
2026-06-29 13:30:20 +0300

    tree

--- algos/tree/700. Search in a Binary Search Tree- dfs.js
+++ algos/tree/700. Search in a Binary Search Tree- dfs.js
@@ -11,3 +11,22 @@ var searchBST = function(root, val) {
         return searchBST(root.right, val)
     }
 };
+
+var searchBST2 = function (root, val) {
+
+    const stack = [root]
+
+    while (stack.length) {
+        const lastEl = stack.pop()
+
+        if (!lastEl) return null;
+
+        if (lastEl.val === val) return lastEl;
+
+        if (val < lastEl.val) {
+            stack.push(lastEl.left)
+        } else {
+            stack.push(lastEl.right)
+        }
+    }
+};
