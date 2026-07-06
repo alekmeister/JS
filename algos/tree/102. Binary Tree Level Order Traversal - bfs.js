5a287640e8073ca0ffab0f22607426d2160be380
Alekmeister <olegpro6@gmail.com>
2026-07-06 13:50:03 +0300

    tree

--- algos/tree/102. Binary Tree Level Order Traversal - bfs.js
+++ algos/tree/102. Binary Tree Level Order Traversal - bfs.js
@@ -1,24 +1,28 @@
 // https://leetcode.com/problems/binary-tree-level-order-traversal/description/
 
-var levelOrder = function(root) {
-    if(!root) return []
+var levelOrder = function (root) {
+    if (!root) return []
 
     const queue = [root]
     const result = []
-    while(queue.length) {
+
+    while (queue.length) {
         const levelResult = []
         const levelSize = queue.length
 
-        for(let i = 0; i < levelSize; i++) {
+        for (let i = 0; i < levelSize; i++) {
             const node = queue.shift()
+
             levelResult.push(node.val)
 
-            if(node?.left) queue.push(node.left);
-            if(node?.right) queue.push(node.right);
+            if (node?.left) queue.push(node.left);
+            if (node?.right) queue.push(node.right);
         }
         result.push(levelResult)
     }
-
     return result
 };
+
+
+
 
