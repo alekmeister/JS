33c3c627407caa3b67d50916e93d806d23e6d366
Alekmeister <olegpro6@gmail.com>
2026-07-10 13:13:52 +0300

    dfs

--- algos/tree/1325. Delete Leaves With a Given Value - dfs.js
+++ algos/tree/1325. Delete Leaves With a Given Value - dfs.js
@@ -11,3 +11,35 @@ var removeLeafNodes = function(root, target) {
     }
     return root
 };
+
+
+import {buildTree} from "./utils.js";
+
+const checkTargetLeaf = (node, target) => {
+    return node.val === target && !node.left && !node.right
+}
+
+var removeLeafNodes2 = function (root, target) {
+    // node side  parent, isVisited
+    const stack = [[root, null, null, false]]
+
+    while (stack.length) {
+        const [node, side, parent, isVisited] = stack.pop()
+        if (!isVisited) {
+            stack.push([node, side, parent, true])
+            if (node?.left) stack.push([node.left, 'left', node, false])
+            if (node?.right) stack.push([node.right, 'right', node, false])
+            continue
+        }
+        if (checkTargetLeaf(node, target) && parent) {
+            parent[side] = null
+        }
+    }
+
+    return checkTargetLeaf(root, target) ? null : root
+};
+
+
+const tree = buildTree([1,2,3,2,null,2,4])
+
+removeLeafNodes2(tree)
