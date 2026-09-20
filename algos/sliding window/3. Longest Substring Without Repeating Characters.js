b6f1f28baf2a7902206f01117faa65aa1fbc2b33
Alekmeister <olegpro6@gmail.com>
2026-09-20 14:01:32 +0300

    sliding window

---
+++ algos/sliding window/3. Longest Substring Without Repeating Characters.js
@@ -0,0 +1,19 @@
+var lengthOfLongestSubstring = function(s) {
+    if(!s) return 0;
+
+    const seen = new Set(); //p w
+    let left = 0;
+    let maxLength = 0;
+
+    for (let right = 0; right < s.length; right++) {
+        while (seen.has(s[right])) {
+            seen.delete(s[left]);
+            left++;
+        }
+        seen.add(s[right]);
+        maxLength = Math.max(maxLength, right - left + 1);
+    }
+
+    return maxLength;
+};
+
